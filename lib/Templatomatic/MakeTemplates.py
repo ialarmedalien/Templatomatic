f67a87248d06d51afaa6647e0e1c2e9eb6b8727d
ialarmedalien <ialarmedalien@gmail.com>
2020-05-22 17:25:28 -0700

    First draft

---
+++ lib/Templatomatic/MakeTemplates.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+import logging
+import os
+import json
+from shutil import copy, copytree
+from installed_clients.KBaseReportClient import KBaseReport
+
+class MakeTemplates:
+    '''
+    Module Name:
+    MakeTemplates
+
+    Module Description:
+    A module for making templates
+    '''
+
+    def __init__(self, config):
+        self.callback_url = os.environ['SDK_CALLBACK_URL']
+        self.scratch = config['scratch']
+        self.appdir = config['appdir']
+        logging.basicConfig(
+            format='%(created)s %(levelname)s: %(message)s',
+            level=logging.INFO
+        )
+        self.reporter = KBaseReport(self.callback_url, service_ver='dev')
+
+
+    def make_templates(self, params):
+        """
+        This example function accepts any number of parameters and returns results in a KBaseReport
+        :param params: instance of mapping from String to unspecified object
+        :returns: instance of type "ReportResults" -> structure: parameter
+           "report_name" of String, parameter "report_ref" of String
+        """
+        # ctx is the context object
+        # return variables are: output
+
+        # copy the templates into 'scratch', where they can be accessed by KBaseReport
+        copytree(
+            os.path.join(self.appdir, 'templates'),
+            os.path.join(self.scratch, 'templates')
+        )
+
+        html_links = [
+            self.report_in_directory(),
+            self.standalone_report_from_tsv(),
+        ]
+
+        return self.reporter.create_extended_report({
+            'html_links': html_links,
+            'direct_html_link_index': 0,
+            'report_object_name': 'Report in Directory',
+            'workspace_name': params['workspace_name']
+        })
+
+    def standalone_report_from_tsv(self):
+        '''
+        render a report from some standalone data
+        '''
+
+        source_file = os.path.join(self.appdir, 'data', 'edge_data.tsv')
+
+        # read in a file, splitting on '\n' and then '\t' to create a list of lists
+        with open(source_file, 'r') as read_fh:
+            lines = list(map(lambda x: x.split('\t'), read_fh.read().rstrip().split('\n')))
+
+        tmpl_data = {
+            # lines[0] is the header
+            # the rest of the lines are the data points
+            'parsed_data_from_file': lines[1:],
+        }
+
+        return {
+            'name': 'standalone_report.html',
+            'template': {
+                'template_file': os.path.join(self.scratch, 'templates', 'edge_data_array.tt'),
+                'template_data_json': json.loads(tmpl_data),
+            },
+            'description': 'HTML report with data from controller',
+        }
+
+    def report_in_directory(self):
+        '''
+        render a report in a directory with supporting files
+        '''
+
+        # copy the templates into the scratch directory
+        output_dir = os.path.join(self.scratch, 'output_dir')
+        os.makedirs(output_dir)
+
+        # first render the report
+        self.reporter.render_template({
+            'template_file': os.path.join(self.scratch, 'templates', 'edge_data_tsv_file.tt'),
+            'template_data_json': json.loads({
+                'file_path': 'edge_data.tsv',
+            }),
+            'output_file': os.path.join(output_dir, 'report.html')
+        })
+
+        # copy any supporting files into the directory
+        # these might also include images, JS, or CSS files needed to render the page,
+        # as well as data files
+        copy(
+            os.path.join(self.appdir, 'data', 'edge_data.tsv'),
+            os.path.join(output_dir, 'edge_data.tsv')
+        )
+
+        # add the directory containing the report and supporting files to html_links
+        return {
+            'path': output_dir,
+            'name': 'report.html',
+            'description': 'HTML report in directory with supporting files',
+        }
+
---
+++ lib/Templatomatic/TemplatomaticImpl.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+#BEGIN_HEADER
+import logging
+import os
+import MakeTemplates
+#END_HEADER
+
+
+class Templatomatic:
+    '''
+    Module Name:
+    Templatomatic
+
+    Module Description:
+    A KBase module: Templatomatic
+    '''
+
+    ######## WARNING FOR GEVENT USERS ####### noqa
+    # Since asynchronous IO can lead to methods - even the same method -
+    # interrupting each other, you must be *very* careful when using global
+    # state. A method could easily clobber the state set by another while
+    # the latter method is running.
+    ######################################### noqa
+    VERSION = "0.0.1"
+    GIT_URL = ""
+    GIT_COMMIT_HASH = ""
+
+    #BEGIN_CLASS_HEADER
+    #END_CLASS_HEADER
+
+    # config contains contents of config file in a hash or None if it couldn't
+    # be found
+    def __init__(self, config):
+        #BEGIN_CONSTRUCTOR
+        self.callback_url = os.environ['SDK_CALLBACK_URL']
+        self.shared_folder = config['scratch']
+        self.templater = MakeTemplates(config)
+        logging.basicConfig(format='%(created)s %(levelname)s: %(message)s',
+                            level=logging.INFO)
+        #END_CONSTRUCTOR
+        pass
+
+
+    def run_Templatomatic(self, ctx, params):
+        """
+        This example function accepts any number of parameters and returns results in a KBaseReport
+        :param params: instance of mapping from String to unspecified object
+        :returns: instance of type "ReportResults" -> structure: parameter
+           "report_name" of String, parameter "report_ref" of String
+        """
+        # ctx is the context object
+        # return variables are: output
+        #BEGIN run_Templatomatic
+        output = self.templater.make_templates(params)
+        #END run_Templatomatic
+
+        # At some point might do deeper type checking...
+        if not isinstance(output, dict):
+            raise ValueError('Method run_Templatomatic return value ' +
+                             'output is not type dict as required.')
+        # return the results
+        return [output]
+    def status(self, ctx):
+        #BEGIN_STATUS
+        returnVal = {'state': "OK",
+                     'message': "",
+                     'version': self.VERSION,
+                     'git_url': self.GIT_URL,
+                     'git_commit_hash': self.GIT_COMMIT_HASH}
+        #END_STATUS
+        return [returnVal]
